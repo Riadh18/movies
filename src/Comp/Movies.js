c253a171403cd55f9dd3d955c269600e1b257b6b
Riadh18 <allaniriadh103@gmail.com>
2022-12-20 23:07:44 -1100

    first commit

---
+++ src/Comp/Movies.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+const Movies=()=>{
+    return(
+        
+        
+       <div className="avatar">
+        <img src="mo.png"alt="not found" width="300"height="300"></img>
+        <h6>Avatar the way of water </h6>
+        <h6>Director:James Cameron</h6>
+        <a href="https://tv.cima4uu.cfd/Video/Avatar++The+Way+of+Water+2022-53192.html">WATCH NOW !</a>
+        <div/>
+
+
+        <div className="Skyscraper ">
+        <img src="sky.png"alt="not found" width="300" height="300"></img> 
+        <h6>Skyscraper </h6>
+        <h6>Director:Rawson Marshall Thurber</h6>
+        <a href="https://tv.cima4uu.cfd/Video/Skyscraper+2018-31199.html">WATCH NOW !</a>
+        </div>
+        
+
+        <div className="Leon">
+            <img src="leo.png"alt="not found" width="300"height="300"></img>
+            <h6>leon the professional</h6>
+            <h6>director:Luc Besson</h6>
+            <a href="https://tv.cima4uu.cfd/Video/Leon+the+Professional+1994-10611.html">WATCH NOW ! </a>
+                 </div>
+                 </div>
+
+        
+
+    
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+    
+    )
+}
+
+export default Movies;
